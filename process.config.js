5e3436a2e30825c7b9f7157663c49595f34be6ad
Sergey Sova <i.am@lestad.net>
2017-11-24 01:18:25 +0300

    Add env for pm2

--- process.config.js
+++ process.config.js
@@ -6,6 +6,12 @@ module.exports = {
       exec_mode: 'fork',
       instances: 1,
       watch: false,
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 }
