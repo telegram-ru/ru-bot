c6b648b08960a22912e1d51df3b4b1c8dcc16e95
Sergey Sova <i.am@lestad.net>
2017-12-03 18:03:39 +0300

    Add todo tests files to add files to coverage

---
+++ src/features/bot-participation/bot-participation.test.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+
+import featureBotParticipation from './'
+
+
+test.todo('featureBotParcipation')
---
+++ src/lib/hammer.test.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+
+import hammer from './hammer'
+
+
+test.todo('hammer')
