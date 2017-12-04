16bd78673093a620649cbf0e78ecf75cf88a0a4f
Sergey Sova <i.am@lestad.net>
2017-12-04 08:04:33 +0300

    Change id for elastic to more complex

--- src/lib/runtime.js
+++ src/lib/runtime.js
@@ -54,7 +54,7 @@ function createBot(token, botanioToken, features, telegrafConfig = {}) {
         push({
           index: `rubot-${process.env.NODE_ENV || 'undefined'}`,
           type: 'message',
-          id: String(ctx.update.message.message_id),
+          id: `${ctx.update.message.message_id}-${ctx.update.chat.id}-${ctx.update.from.id}`,
           body: ctx.update.message,
         }).catch((error) => {
           console.error('Cant push to elastic', error) // eslint-disable-line no-console
