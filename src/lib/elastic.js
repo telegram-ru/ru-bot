ddb0c2f690d6a4f9cd2e40778bb9eff18bd6728a
Sergey Sova <i.am@lestad.net>
2017-12-04 07:14:03 +0300

    Add elastic log

---
+++ src/lib/elastic.js
@@ -0,0 +1,42 @@
+const debug = require('debug')('rubot:lib:elastic')
+const elasticsearch = require('elasticsearch')
+
+
+let client = null
+
+function elasticPing() {
+  debug('elasticPing()')
+
+  if (!client) {
+    client = new elasticsearch.Client({
+      host: 'localhost:9200',
+      log: 'trace',
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    client.ping({ requestTimeout: 500 }, (error) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve()
+    })
+  })
+}
+
+
+function push({ index, type, id, body, timestamp }) {
+  debug('push(', { index, type, id, timestamp }, ')')
+  return new Promise((resolve, reject) => {
+    client.create({ index, type, id, body, timestamp }, (error, response) => {
+      if (error) reject(error)
+      else resolve(response)
+    })
+  })
+}
+
+module.exports = {
+  client,
+  push,
+  elasticPing,
+}
