76c14a1b8a7fba1584b9e94250d176108f635bfa
Shelomanov Dmitry <userbq201@mail.ru>
2018-03-18 22:09:43 +0300

    feat(!status): add feat status
    ISSUES CLOSED: #52

--- .cz-config.js
+++ .cz-config.js
@@ -18,6 +18,7 @@ module.exports = {
     { name: '!private-greetings' },
     { name: '!readonly-mode' },
     { name: '!spam-hammer' },
+    { name: '!status' },
     { name: 'index' },
     { name: 'lib' },
     { name: 'middlewares' },
